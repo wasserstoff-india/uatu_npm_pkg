1e79877d44670f6eb993f6484aed0b51a33b5580
wasserstoffAbhi <abhishek@thewasserstoff.com>
2023-01-19 23:19:48 +0530

    Tried Bundel

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+export default {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'index_bundle.js',
+    path: "/home/wsft02/source_files/uatu_npm_pkg/dist",
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    libraryExport: 'default',
+    umdNamedDefine: true,
+    library: 'MyLibrary',
+  }
+};
